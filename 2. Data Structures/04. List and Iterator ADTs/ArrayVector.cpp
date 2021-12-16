98566a1ad3c17048c92f4472266c531c126e5f2e
Lee Dong-geon <peter8739@gmail.com>
2021-12-16 14:12:50 +0900

    2021. 12. 16. Update Array Vector

--- 2. Data Structures/04. List and Iterator ADTs/ArrayVector.cpp
+++ 2. Data Structures/04. List and Iterator ADTs/ArrayVector.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-// #include <algorithm>
-// using std::max;
 using namespace std;
 
 #define throw(IndexOutOfBounds)
@@ -32,11 +30,9 @@ public:
     bool empty() const;                      // is vector empty?
     Elem &operator[](int i);                 // element at index
     Elem &at(int i) throw(IndexOutOfBounds); // element at index
-    void set(int i, const Elem &e);          // set element at index
     void erase(int i);                       // remove element at index
     void insert(int i, const Elem &e);       // insert element at index
-    void insertDoubling(int N);              // insertDoubling at least N spots
-    void insertIncremental(int N);           // insertIncremental at least N spots
+    void reserve(int N);                     // reserve at least N spots
     // . . . (housekeeping functions omitted)
 private:
     int capacity; // current array size
@@ -78,20 +74,8 @@ void ArrayVector::erase(int i)
 }
 
 // Code Fragment 6.5: The member functions insertDoubling and insert for class ArrayVector.
-void ArrayVector::insertDoubling(int N)
-{ // insertDoubling at least N spots
-    if (capacity >= N)
-        return;                         // already big enough
-    Elem *B = new Elem[capacity + 100]; // allocate bigger array
-    for (int j = 0; j < n; j++)         // copy contents to new array
-        B[j] = A[j];
-    if (A != NULL)
-        delete[] A; // discard old array
-    A = B;          // make B the new array
-    capacity = N;   // set new capacity
-}
-void ArrayVector::insertIncremental(int N)
-{ // insertIncremental at least N spots
+void ArrayVector::reserve(int N)
+{ // reserve at least N spots
     if (capacity >= N)
         return;                 // already big enough
     Elem *B = new Elem[N];      // allocate bigger array
@@ -105,57 +89,10 @@ void ArrayVector::insertIncremental(int N)
 
 void ArrayVector::insert(int i, const Elem &e)
 {
-    if (n >= capacity)                        // overflow?
-        insertDoubling(max(1, 2 * capacity)); // double array size
-    for (int j = n - 1; j >= i; j--)          // shift elements up
+    if (n >= capacity)                 // overflow?
+        reserve(max(1, 2 * capacity)); // double array size
+    for (int j = n - 1; j >= i; j--)   // shift elements up
         A[j + 1] = A[j];
     A[i] = e; // put in empty slot
     n++;      // one more element
-}
-
-void ArrayVector::set(int i, const Elem &e)
-{
-    if (n >= capacity)                        // overflow?
-        insertDoubling(max(1, 2 * capacity)); // double array size
-    A[i] = e;                                 // set element
-}
-
-int main()
-{
-    ArrayVector myV;
-    // Your codes go here to do (c)--(e)
-    try
-    {
-        if (myV.empty() == true)
-        {
-            myV.insert(0, 'A');
-            myV.erase(0);
-            myV.insert(0, 'I');
-            myV.insert(1, 'E');
-            myV.insert(1, 'C');
-            myV.insert(3, '1');
-            myV.set(3, '2');
-            myV.insert(4, '0');
-            myV.insert(5, '0');
-            myV.insert(6, '4');
-
-            for (int i = 0; i < myV.size(); i++)
-            {
-                cout << myV.at(i) << endl;
-            }
-
-            int index = 100;
-
-            if (index > myV.size())
-            {
-                throw IndexOutOfBounds("illegal index in function at()");
-            }
-        }
-    }
-    catch (IndexOutOfBounds)
-    {
-        cout << "Invalid Index!" << endl;
-    }
-
-    return 0;
 }
